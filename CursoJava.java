3e7a940ffa0da04207d93d0a4d0450fb27744b2a
vandersonbfs <vanderson_b@hotmail.com>
2022-11-06 20:32:38 -0300

    Comandos de formatação

--- CursoJava.java
+++ CursoJava.java
@@ -7,6 +7,16 @@ public class CursoJava {
 
         System.out.print("CFB Curso\n");
         System.out.println("Curso de Java.");
+
+        /*
+         * Printf:
+         * Comandos de formatação
+         * String %s
+         * Inteiro %d
+         * 
+         * 
+         * 
+         */
         System.out.printf("%s - %d", "CFB Cursos", 2021);
 
     }
