105f3d8b81756f8e4facae75a80d18d6705ba49b
vandersonbfs <vanderson_b@hotmail.com>
2022-11-06 20:27:58 -0300

    CursoJavaCFBCursos

---
+++ CursoJava.java
@@ -0,0 +1,12 @@
+public class CursoJava {
+    public static void main(String[] args) {
+        /*
+         * Aula 02 comandos de saida:
+         * 
+         */
+
+        System.out.print("CFB Curso\n");
+        System.out.println("Curso de Java.");
+        System.out.printf("%s - %d", "CFB Cursos", 2021);
+    }
+}
